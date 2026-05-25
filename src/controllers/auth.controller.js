12718d36feff8d8e63949891a331b15a012278c8
akshat-nawaria <akshatnawaria804@gmail.com>
2026-05-25 10:52:59 +0530

    feat: integrate Nodemailer with OAuth2 to send welcome email upon registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken");
+const emailService = require("../services/email.service")
+
+
 /** 
 * - user register controller
 * - POST /api/auth/register
@@ -34,9 +37,50 @@ async function userRegisterController(req,res){
         },
         token
     })
+
+    await emailService.sendRegistrationEmail(user.email, user.name);
+}
+
+/**
+ * - user login controller
+ * - POST /api/auth/login 
+ * */ 
+
+async function userLoginController(req,res){
+    const {email, password} = req.body
+
+    const user = await userModel.findOne({email}).select("+password")
+
+    if(!user){
+        return res.status(401).json({
+            message : "Email or Password is INVALID"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password)
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message : "Email or Password is INVALID"
+        })
+    }
+
+    const token = jwt.sign({userID:user._id}, process.env.JWT_SECRET, {expiresIn: "3d"})
+    res.cookie("token", token);
+
+    res.status(200).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })
 }
 
 
+
 module.exports = {
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const authController = require("../controllers/auth.controller")
 const router = express.Router()
 
-// POST /api/auth/register -> complete API
+// POST /api/auth/register -> Register User
 router.post("/register", authController.userRegisterController)
+
+// POST /api/auth/login -> Login User
+router.post("/login", authController.userLoginController)
+
 
 module.exports = router
